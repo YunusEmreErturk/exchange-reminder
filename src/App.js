1f98db9cd5bd7a398421dc8779ece90f29e25dd2
YunusEmreErturk <yemrerturk@gmail.com>
2020-08-21 23:42:25 +0300

    getExchanges added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import HomePage from "./components/HomePage";
 import HomePageNavbar from "./components/Navbar";
 import { Container } from "reactstrap";
 
-class App extends Component  {
+class App extends Component {
+  state = {
+    exchanges: [],
+  };
+
+  getExchanges = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((result) => this.setState({ exchanges: result }));
+  };
+  componentDidMount(){
+    this.getExchanges();
+  }
   render() {
     return (
       <Container className="mt-4">
-        <HomePageNavbar/>
-        <HomePage/>
+        <HomePageNavbar />
+        <HomePage />
       </Container>
     );
   }
