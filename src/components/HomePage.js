6b199910c3a1824f22280262f63bf3ee40e11d0f
YunusEmreErturk <yemrerturk@gmail.com>
2020-08-21 22:35:21 +0300

    App components changed

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import {Container} from "reactstrap"
+export default class home extends Component {
+    render() {
+        return (
+            <div>
+                    Initial Commit
+            </div>
+        )
+    }
+}
